052905e285ee5690a9af02f1883738162caeeefe
taaesan <taaesan@gmail.com>
2021-07-24 08:03:30 +0700

    init project

---
+++ src/main/kotlin/com/taaesan/kotlinbackend/KotlinBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.taaesan.kotlinbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinBackendApplication>(*args)
+}
