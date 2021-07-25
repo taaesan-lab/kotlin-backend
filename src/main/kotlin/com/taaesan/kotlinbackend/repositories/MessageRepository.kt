d05a8e2f1f70a35e6637484bf8ddb47121d92731
taaesan <taaesan@gmail.com>
2021-07-25 09:29:17 +0700

    Add repository

---
+++ src/main/kotlin/com/taaesan/kotlinbackend/repositories/MessageRepository.kt
@@ -0,0 +1,11 @@
+package com.taaesan.kotlinbackend.repositories
+
+import com.taaesan.kotlinbackend.models.Message
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.repository.CrudRepository
+
+interface MessageRepository : CrudRepository<Message, String> {
+
+    @Query("select * from messages")
+    fun findMessages(): List<Message>
+}
